6f72697cc62a79b214bd76ce193cc9d527f4c86b
Abhijit <128697608+AbhijitGhate1995@users.noreply.github.com>
2024-03-31 13:53:09 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import DetailsFilling from "./Pages/DetailsFilling";
+import CheckSelectedId from "./Components/CheckSelectedId";
+import AboutUs from "./Pages/AboutUs";
+import MyResumes from "./Pages/MyResumes";
+import DashboardPage from "./Pages/dashboard";
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Home Page Route */}
+        <Route exact path="/dashboard" element={<Home />} />
+        <Route exact path="/" element={<DashboardPage />} />
+
+
+        {/* Details Filling Page */}
+        <Route
+          exact
+          path="/template/fill-details"
+          element={
+            <CheckSelectedId>
+              <DetailsFilling />
+            </CheckSelectedId>
+          }
+        />
+
+        {/* My Resumes Page */}
+        <Route exact path="/my/resumes" element={<MyResumes />} />
+
+        {/* About Us Page */}
+        <Route exact path="/about-us" element={<AboutUs />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
